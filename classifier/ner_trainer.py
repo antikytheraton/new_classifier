4566465734c20bd3ffff9a31ae0f3a97d3e28fcf
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-08-17 18:26:49 -0500

    ajustando la SVM

--- classifier/ner_trainer.py
+++ classifier/ner_trainer.py
@@ -3,14 +3,10 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.externals import joblib
 from sklearn import svm, model_selection
 from sklearn.metrics import accuracy_score
-from sklearn.svm import SVC
-import numpy as np
 
 df=pd.read_csv("../dataset/data_final_inflado_tag.csv")
 tags=df['tags'].tolist()[1:]
-# print(list(enumerate(tags)))
 words=df['words'].tolist()[1:]
-# print(words)
 
 def prepara_frase(tags,words):
     features=[]
@@ -36,18 +32,9 @@ joblib.dump(vectorizer, 'vectorizer_entity.pkl')
 
 
 transformed=vectorizer.transform(features)
-# print(features)
-# print(targets)
-#X_train, X_test, y_train, y_test = cross_validation.train_test_split(transformed, targets, test_size=0.1, random_state=42) #Se intenta quitar por deprecated warning
-X_train, X_test, y_train, y_test = model_selection.train_test_split(transformed, targets, test_size=0.10, random_state=42)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(transformed, targets, test_size=0.20, random_state=42)
 
-# print(np.isnan(X_train).any())
-# np.isnan(y_train).any()
-# np.isnan(X_test).any()
-# np.isnan(y_test).any()
-# X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=42)
-# print(y_test)
-lin_svc = svm.LinearSVC(C=1).fit(X_train, y_train)
-# svm_machine = svm.SVC(probability=True).fit(X_train, y_train)
-# print(str(accuracy_score(y_test, lin_svc.predict(X_test))))
-# joblib.dump(lin_svc, 'clasifier_entity.pkl')
+lin_svc = svm.LinearSVC(C=100).fit(X_train, y_train)
+print(str(accuracy_score(y_test, lin_svc.predict(X_test))))
+
+joblib.dump(lin_svc, 'classifier_entity.pkl')
